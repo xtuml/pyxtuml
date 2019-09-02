c78932ac03b52ce14af993f4fca4e64a74b93e21
John Törnblom <john.tornblom@gmail.com>
2019-09-02 20:47:33 +0200

    version bump

--- xtuml/version.py
+++ xtuml/version.py
@@ -17,8 +17,8 @@
 # License along with pyxtuml. If not, see <http://www.gnu.org/licenses/>.
 
 name = 'pyxtuml'
-date = '2018-07-21'
-version = '2.1'
-release = '2.1.0'  # ensure that this is the same as in setup.py
+date = '2019-09-02'
+version = '2.2'
+release = '2.2.0'  # ensure that this is the same as in setup.py
 
 complete_string = '%s v%s (%s)' % (name, release, date)
