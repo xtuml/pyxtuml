a288c7eb0c93c3efc653806dcf4402e1959c2301
tlondon-techmod <travis.london@gmail.com>
2020-10-07 12:27:08 -0500

    Job: 12009
    Add more path data for INFO logs during action processing.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class TestCommand(Command):
 
 
 setup(name='pyxtuml',
-      version='2.2.1', # ensure that this is the same as in xtuml.version
+      version='2.2.2', # ensure that this is the same as in xtuml.version
       description='Library for parsing, manipulating, and generating BridgePoint xtUML models',
       author=u'John Törnblom',
       author_email='john.tornblom@gmail.com',
