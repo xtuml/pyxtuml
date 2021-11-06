8795853d07982f87ec684c66760b2485a5cd85a3
Levi Starrett <levi@roxsoftware.com>
2021-11-05 22:46:04 -0400

    fixup bump version for testing

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ with open(path.join(this_directory, 'README.rst')) as f:
     long_description = f.read()
 
 setup(name='pyxtuml-test',
-      version='2.2.4', # ensure that this is the same as in xtuml.version
+      version='2.2.7', # ensure that this is the same as in xtuml.version
       description='Library for parsing, manipulating, and generating BridgePoint xtUML models',
       long_description=long_description,
       long_description_content_type='text/x-rst',
